ab3db1cdf74ce22a2c2df9f6300b134aed47c653
Victor Martins Soares <vms5@ug4c45.windows.cin.ufpe.br>
2018-12-08 08:02:02 -0300

    alteracoes questao 8

--- 8-sum.hs
+++ 8-sum.hs
@@ -3,19 +3,20 @@ sub [] = [[]]
 sub (x:xs) = sub1 ++ map (x:) sub1
     where sub1 = sub xs
 
-soma :: [Int] -> Int
-soma [] = 0
-soma (x:xs) = x + soma xs
+--soma :: [Int] -> Int
+--soma [] = 0
+--soma (x:xs) = x + soma xs
 
 funcao :: [[Int]] -> Int -> Int
-funcao [[]] _ = 0
+--funcao [[]] _ = 0
 funcao (x:xs) s
- | soma x == s = 1+funcao xs
- | otherwise = funcao xs 
+ | length(x:xs) == 1 = length x
+ | sum x == s = 1+(funcao xs s)
+ | otherwise = funcao xs s
 
 main :: IO ()
 main = do
     a <- getLine
     l <- readLn
     let ints = map read (words a) :: [Int]
-    putStrLn(show (funcao l (sub ints)))
+    putStrLn(show (funcao (sub ints) l))
