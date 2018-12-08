4a4deaa5cf4d68a1b9f849abcbf8c7dbc0abda22
Victor Martins Soares <vms5@ug4c45.windows.cin.ufpe.br>
2018-12-08 18:23:55 -0300

    ultimo

--- exemplo.hs
+++ exemplo.hs
@@ -2,6 +2,11 @@ import Control.Monad
 
 main :: IO ()
 main = do
-    input <- getLine
-    inputs <- replicateM (read input) getLine
-    print $ length inputs
+    nlinhas <- getLine
+    ncolunas <- getLine
+    inputs <- replicateM (read nlinhas) getLine
+    putStrLn(show(inputs))
+    --print (length inputs)
+
+    --map read $ words "1 2 3 4 5" :: [Int]
+--[1,2,3,4,5]
